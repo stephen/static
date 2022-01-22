e09711a241e75645923969a01082b83f334a47d3
Yolanda Zhou <yolanda.zhou@samsara.com>
2022-01-21 16:19:34 -0800

    city-guide: add home page images

--- city-guide/pages/index.tsx
+++ city-guide/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
 import type { GetStaticPathsResult, GetStaticPropsContext, InferGetStaticPropsType } from "next";
+import { getImgPrefix } from "./utils";
 
 import { getCities } from "../lib/api";
 
@@ -13,6 +14,7 @@ export async function getStaticProps() {
 }
 
 const Home = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const imgPrefix = getImgPrefix();
   return (
     <div>
       <Head>
@@ -22,11 +24,27 @@ const Home = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Tinos:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
       </Head>
-      <section className={styles.cities}>
-        {props.cities.map(city =>
-          <Link key={city.abbr} href={`/${city.abbr}`}><a className={styles.cityLink}>{city.name}</a></Link>
-        )}
-      </section>
+      <div className={styles.container}>
+        <h1 className={styles.heading}>City Guide</h1>
+        <section className={styles.cities}>
+          {props.cities.map((city) => (
+            <>
+              <Link key={city.abbr} href={`/${city.abbr}`}>
+                <div className={styles.city}>
+                  <a
+                    className={styles.cityImage}
+                    style={{
+                      backgroundImage: `url("${imgPrefix}/${city.abbr}.jpeg")`,
+                      backgroundPosition: city.imgPosition,
+                    }}
+                  />
+                  <span>{city.name}</span>
+                </div>
+              </Link>
+            </>
+          ))}
+        </section>
+      </div>
     </div>
   );
 }
