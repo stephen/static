1fea28933ea20b6c78fe5e65e13bdbd0f3e322cb
Stephen Wan <stephen@stephenwan.com>
2023-07-28 17:46:14 -0400

    quiz-proto: add

--- bigfuncloud/build.sh
+++ bigfuncloud/build.sh
@@ -10,3 +10,12 @@ npm run build
 npm run export
 popd
 cp -r ./city-guide/out/. ./out/city-guide
+
+pushd quiz-proto
+npm install
+npm run build-production
+popd
+cp -r ./quiz-proto/out/. ./out/quiz-proto
+cp -r ./quiz-proto/src/index.html ./out/quiz-proto
+cp -r ./quiz-proto/node_modules/kuromoji/dict/ ./out/quiz-proto/dict/
+
